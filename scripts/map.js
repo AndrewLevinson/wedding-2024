2555083ee7730e0cc78fb8d8be9f8a9babc166a7
Andrew Levinson <andrewlevins@gmail.com>
2023-12-26 23:19:42 -0500

    map updates

--- scripts/map.js
+++ scripts/map.js
@@ -3,17 +3,27 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   center: [-73.975, 40.725], // starting position [lng, lat]
-  zoom: 12, // starting zoom
+  zoom: 11, // starting zoom
   style: 'mapbox://styles/andrewlevinson/clqk3wbgr00fg01pi41z66p8y',
+  projection: 'mercator',
 });
 
-map.on('load', function () {
+map.on('load', 'our-nyc', function (e) {
   const tooltip = document.querySelector('.tooltip');
+  const bounds = new mapboxgl.LngLatBounds();
+  const $list = document.querySelector('.list');
+  e.features.forEach(feature => {
+    const { name } = feature.properties;
+    $list.innerHTML += `<li>${name}</li>`;
+    bounds.extend(feature.geometry.coordinates);
+  });
+  map.fitBounds(bounds, { padding: 100 });
+
   map.on('mousemove', 'our-nyc', e => {
     const props = e.features[0].properties;
-    const { name, description } = props;
+    const { name, description, vibe } = props;
     tooltip.style.opacity = 1;
-    tooltip.innerHTML = `<div class="name">${name}</div> <div class="description">${description}</div>`;
+    tooltip.innerHTML = `<div class="name">${name}</div> <div class="description">${description}</div><div class="vibe">${vibe}</div>`;
     tooltip.style.top = `${e.point.y + 30}px`;
     tooltip.style.left = `${e.point.x - 112}px`;
   });
