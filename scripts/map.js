140b83b95a228db5a3f99dc47d37afbd5140c5b2
Andrew Levinson <andrewlevins@gmail.com>
2024-04-21 14:05:15 -0400

    updated about us images and map functionality. committed with commented out code in order to revert if needed

--- scripts/map.js
+++ scripts/map.js
@@ -1,27 +1,37 @@
+const locationData = {
+  oakville: [-79.6877, 43.4675],
+  queens: [-73.7692, 40.7603],
+  bg: [-87.9631, 42.1663],
+  chicago: [-87.6488, 41.9255],
+  brooklyn: [-73.9571, 40.7081],
+  toronto: [-79.3832, 43.6532],
+  vancouver: [-123.1207, 49.2827],
+  manhattan: [-73.9911, 40.7359],
+};
+
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW5kcmV3bGV2aW5zb24iLCJhIjoiY2pub3RxNXB2MDA5cTNxb2M5MjNoaHl5diJ9.Zq4eS5UJd_60fgNBAFiUsw';
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   center: [-73.975, 40.725], // starting position [lng, lat]
-  zoom: 11, // starting zoom
+  // center: [-73.7692, 40.7603],
+  zoom: 10, // starting zoom
   style: 'mapbox://styles/andrewlevinson/clqk3wbgr00fg01pi41z66p8y',
   projection: 'mercator',
+  interactive: false,
 });
 
 const scroller = scrollama();
 
 map.on('load', 'our-nyc', ourLayer => {
-  map.scrollZoom.disable();
+  // map.scrollZoom.disable();
   const tooltip = document.querySelector('.tooltip');
   const bounds = new mapboxgl.LngLatBounds();
-  const $empyList = document.querySelector('.list');
+
   const features = ourLayer.features;
-  $empyList.innerHTML = '';
-  features.forEach(feature => {
-    const { name } = feature.properties;
-    $empyList.innerHTML += `<li data-id="${feature.id}">${name}</li>`;
-    bounds.extend(feature.geometry.coordinates);
-  });
+  console.log('loaded');
+  // const $empyList = document.querySelector('.list');
+
   // map.fitBounds(bounds, { padding: 75 });
 
   const handleMouseMove = (e, foundFeature) => {
@@ -44,19 +54,6 @@ map.on('load', 'our-nyc', ourLayer => {
     handleMouseOut();
   });
 
-  const $finishedList = document.querySelectorAll('.list li');
-  $finishedList.forEach(el => {
-    el.addEventListener('mouseover', e => {
-      const dataID = e.target.dataset.id;
-      handleMouseMove(
-        null,
-        features.find(feature => feature.id == dataID)
-      );
-    });
-
-    el.addEventListener('mouseout', handleMouseOut);
-  });
-
   // add scroll steps here
   scroller
     .setup({
@@ -64,68 +61,42 @@ map.on('load', 'our-nyc', ourLayer => {
     })
     .onStepEnter(response => {
       const { element, index, direction } = response;
-
       const city = element.dataset?.city;
 
-      switch (city) {
-        case 'oakville':
-          map.flyTo({
-            center: [-79.6877, 43.4675],
-            zoom: 9,
-          });
-          break;
-
-        case 'queens':
-          map.flyTo({
-            center: [-73.7692, 40.7603],
-            zoom: 9,
-          });
-          break;
-
-        case 'bg':
-          map.flyTo({
-            center: [-87.9631, 42.1663],
-            zoom: 9,
-          });
-          break;
-        case 'chicago':
-          map.flyTo({
-            center: [-87.6488, 41.9255],
-            zoom: 9,
-          });
-          break;
-
-        case 'brooklyn':
-          map.flyTo({
-            center: [-73.9571, 40.7081],
-            zoom: 9,
-          });
-          break;
-        case 'toronto':
-          map.flyTo({
-            center: [-79.3832, 43.6532],
-            zoom: 9,
-          });
-          break;
-
-        case 'vancouver':
-          map.flyTo({
-            center: [-123.1207, 49.2827],
-            zoom: 9,
-          });
-          break;
-
-        default:
-          break;
-      }
-
+      map
+        .flyTo({
+          center: locationData[city],
+          zoom: 9,
+          speed: 1.5,
+        })
+        .on('moveend', () => {
+          console.log('running');
+        });
+      // setImgPopup(locationData[city], fileName);
       if (element.dataset.step == 'list' && direction == 'down') {
-        map.fitBounds(bounds, { padding: 75 });
+        // $empyList.innerHTML = '';
+        features.forEach(feature => {
+          // const { name } = feature.properties;
+          // $empyList.innerHTML += `<li data-id="${feature.id}">${name}</li>`;
+          bounds.extend(feature.geometry.coordinates);
+        });
+        map.fitBounds(bounds, { padding: 25 });
+
+        // const $finishedList = document.querySelectorAll('.list li');
+        // $finishedList.forEach(el => {
+        //   el.addEventListener('mouseover', e => {
+        //     const dataID = e.target.dataset.id;
+        //     handleMouseMove(
+        //       null,
+        //       features.find(feature => feature.id == dataID)
+        //     );
+        //   });
+
+        //   el.addEventListener('mouseout', handleMouseOut);
+        // });
       }
     })
     .onStepExit(response => {
       const { element, index, direction } = response;
-      // console.log(element, index, direction);
-      // { element, index, direction }
     });
 });
